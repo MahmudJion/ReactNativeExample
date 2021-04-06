a526ce2f56976a0d072f25a52890cc038c3597ef
MahmudJion <mahmudh.jion02@gmail.com>
2021-04-07 02:44:53 +0600

    list view add with dummy data

--- App.js
+++ App.js
@@ -1,16 +1,58 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
+  FlatList,
   Text,
   StatusBar,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+const DATA = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'fugiat veniam minus',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: 'et porro tempora',
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    completed: false,
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
 const App: () => React$Node = () => {
+  const renderItem = ({item}) => <Item title={item.title} />;
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -25,11 +67,14 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>
-                Hello World from React Native!
-              </Text>
+              <Text style={styles.sectionTitle}>React Native Todo</Text>
             </View>
           </View>
+          <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
         </ScrollView>
       </SafeAreaView>
     </>
@@ -45,16 +90,16 @@ const styles = StyleSheet.create({
     right: 0,
   },
   body: {
-    backgroundColor: Colors.white,
+    backgroundColor: '#232f3e',
   },
   sectionContainer: {
-    marginTop: 32,
+    marginTop: 28,
     paddingHorizontal: 24,
   },
   sectionTitle: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: '600',
-    color: Colors.black,
+    color: Colors.white,
   },
   sectionDescription: {
     marginTop: 8,
@@ -72,6 +117,19 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#fff9f3',
+    padding: 5,
+    marginVertical: 5,
+    marginHorizontal: 10,
+  },
+  title: {
+    fontSize: 32,
   },
 });
 
